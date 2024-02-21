2dc53a24981431cc1793216f85f9413d99b7063c
Yann@SeenKid <30565751+SeenKid@users.noreply.github.com>
2024-02-21 09:16:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
 details : "🌹 AustralisHosting 🌹",
-state: "🌴Europe Game Servers Hosting🌴",
+state: "🌴 Hébergeur Européen 🌴",
 assets : {
 large_image : "logo",
 large_text : "🌹 Australis-Hosting.com 🌹"
 },
-buttons : [{label : "Australis-Hosting WebSite" , url : "https://australis-hosting.com"},{label : "My links", url : "https://links.bunny-hop.net/seenkid"}]
+buttons : [{label : "Site web" , url : "https://australis-hosting.com"},{label : "Liens utiles", url : "https://solo.to/australishosting"}]
 }
 })
 })
